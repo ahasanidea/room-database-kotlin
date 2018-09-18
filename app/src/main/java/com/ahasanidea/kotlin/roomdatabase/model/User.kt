ac602c5fd066ef0bab13aa14bbbd6f921e089901
Zayn An Nisa <shobuz_cse03@yahoo.com>
2018-09-19 00:13:11 +0600

    minor changes

--- app/src/main/java/com/ahasanidea/kotlin/roomdatabase/model/User.kt
+++ app/src/main/java/com/ahasanidea/kotlin/roomdatabase/model/User.kt
@@ -13,17 +13,3 @@ import java.io.Serializable
 data class User(@PrimaryKey(autoGenerate = true) var id:Int?=null, var name:String, var item:String): Serializable{
     constructor():this(null,"","")
 }
-//@Entity
-//class User(var name: String):Serializable {
-//    @PrimaryKey(autoGenerate = true)
-//    var id: Int = 0
-//    lateinit var personName: String
-//    lateinit var itemName: String
-//
-//    constructor(name: String, item: String) : this(name) {
-//        personName = name
-//        itemName = item
-//    }
-//
-//
-//}
